e873ad0c7d7ed9babd52a9214eb9bbab64eae6c2
Rohit Singh Tomar <44311112+rohitsingh30@users.noreply.github.com>
2022-01-18 21:26:13 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-    user: 'postgres',
-    password: 'rohit',
-    database: 'todo_database',
-    host: 'https://app1xapp1.herokuapp.com/',
+    user: 'naykscefaysezd',
+    password: '630c01e8f9c92ef934a7f11e0192d06c41c6740292ca492f1fda63b250e26b54',
+    database: 'd2it9ovdcenne2',
+    host: 'ec2-3-231-69-204.compute-1.amazonaws.com',
     port: 5432
 });
 
