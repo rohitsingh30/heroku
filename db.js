8621c7fb0a6cfd915168371bebd27a75b0788c5f
Rohit Singh Tomar <44311112+rohitsingh30@users.noreply.github.com>
2022-01-18 22:13:59 +0530

    Update db.js

--- db.js
+++ db.js
@@ -5,7 +5,11 @@ const pool = new Pool({
     password: '630c01e8f9c92ef934a7f11e0192d06c41c6740292ca492f1fda63b250e26b54',
     database: 'd2it9ovdcenne2',
     host: 'ec2-3-231-69-204.compute-1.amazonaws.com',
-    port: 5432
+    port: 5432,
+    ssl: {
+    rejectUnauthorized: false
+  }
+
 });
 
 module.exports = pool;
